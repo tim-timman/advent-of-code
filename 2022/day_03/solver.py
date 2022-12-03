a1f095b804128a0b95742731ad279d9bda107581
Tim Johansson <tim.johansson@ecsolutions.se>
2022-12-03 23:52:21 +0100

    Day 3 – Part 2

--- 2022/day_03/solver.py
+++ 2022/day_03/solver.py
@@ -3,17 +3,24 @@ from string import ascii_lowercase, ascii_uppercase
 index_priority = " " + ascii_lowercase + ascii_uppercase
 
 
-def solve(rucksacks: str) -> tuple[int, int | None]:
+def solve(puzzle_data: str) -> tuple[int, int | None]:
+    rucksacks = puzzle_data.splitlines()
+
     priority_sum = 0
-    for r in rucksacks.splitlines():
+    for r in rucksacks:
         half_len = len(r) // 2
         common_type = set(r[:half_len]).intersection(r[half_len:]).pop()
         priority_sum += index_priority.index(common_type)
 
-    return priority_sum, None
+    badge_priority_sum = 0
+    for r1, r2, r3 in zip(*[iter(rucksacks)] * 3, strict=True):
+        badge_type = set(r1).intersection(r2).intersection(r3).pop()
+        badge_priority_sum += index_priority.index(badge_type)
+
+    return priority_sum, badge_priority_sum
 
 
-TEST_EXPECTED = (157, None)
+TEST_EXPECTED = (157, 70)
 TEST_INPUT = """\
 vJrwpWtwJgWrhcsFMMfFFhFp
 jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
