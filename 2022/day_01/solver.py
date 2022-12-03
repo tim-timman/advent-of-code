83d56f308b35ea5eebd660a0b9924c4ed0dec8fd
Tim Johansson <tim.johansson@ecsolutions.se>
2022-12-03 19:55:52 +0100

    Day 1 – Part 2

--- 2022/day_01/solver.py
+++ 2022/day_01/solver.py
@@ -1,8 +1,9 @@
-def solve(puzzle_input: str):
-    return max([
+def solve(puzzle_input: str) -> tuple[int, int]:
+    elf_calories = sorted([
         sum(map(int, foods.split("\n")))
         for foods in puzzle_input.strip().split("\n\n")
-    ])
+    ], reverse=True)
+    return elf_calories[0], sum(elf_calories[:3])
 
 
 TEST_INPUT = """\
@@ -26,11 +27,13 @@ TEST_RESULT = 24000
 
 
 def main():
-    assert solve(TEST_INPUT) == TEST_RESULT
+    assert solve(TEST_INPUT)[0] == TEST_RESULT
 
     from pathlib import Path
     puzzle_input = (Path(__file__).parent / "input.txt").read_text()
-    print(f"Answer part 1: {solve(puzzle_input)}")
+    answer_part_1, answer_part_2 = solve(puzzle_input)
+    print(f"Answer part 1: {answer_part_1}\n"
+          f"Answer part 2: {answer_part_2}")
     return 0
 
 
