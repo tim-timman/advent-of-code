db255960eeeb0f27ca67dfe1f3f2f6f3e2443821
Tim Johansson <tim.johansson@ecsolutions.se>
2022-12-04 11:54:56 +0100

    Day 4 – Part 2
    yuck

--- 2022/day_04/solver.py
+++ 2022/day_04/solver.py
@@ -1,20 +1,28 @@
 def solve(puzzle_input: str) -> tuple[int, int | None]:
     fully_contained_assignments = 0
-
+    num_overlap = 0
     for x in puzzle_input.splitlines():
         assignments = x.split(",")
         a1, a2 = [tuple(map(int, a.split("-"))) for a in assignments]
 
-        for _ in range(2):
-            if a1[0] <= a2[0] and a1[1] >= a2[1]:
-                fully_contained_assignments += 1
-                break
+        contained_counted = False
+        overlap_counted = False
+        for i in range(2):
+            if not overlap_counted:
+                if a1[0] <= a2[0] <= a1[1] or a1[0] <= a2[1] <= a1[0]:
+                    num_overlap += 1
+                    overlap_counted = True
+
+            if not contained_counted:
+                if a1[0] <= a2[0] and a1[1] >= a2[1]:
+                    fully_contained_assignments += 1
+                    contained_counted = True
             a1, a2 = a2, a1
 
-    return fully_contained_assignments, None
+    return fully_contained_assignments, num_overlap
 
 
-TEST_EXPECTED = (2, None)
+TEST_EXPECTED = (2, 4)
 TEST_INPUT = """\
 2-4,6-8
 2-3,4-5
