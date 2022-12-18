3ee51e9df8da1e2d44f0d3a3d8403bcdc28d4ffb
Tim Johansson <tim.johansson@ecsolutions.se>
2022-12-18 14:06:35 +0100

    Day 6 – Part 2

--- 2022/day_06/solver.py
+++ 2022/day_06/solver.py
@@ -1,5 +1,18 @@
+from functools import reduce
+from itertools import islice
+from operator import or_
+
+
 def count_bits(num: int):
     return sum(num & (1 << i) > 0 for i in range(ord("z")))
+
+
+def first_unique_chars(data: list[int], num) -> int | None:
+    offset = num - 1
+    for idx in range(offset, len(data)):
+        val = reduce(or_, islice(data, idx - offset, idx + 1))
+        if count_bits(val) == num:
+            return idx + 1
 
 
 def solve(puzzle_input: str) -> tuple[int, int | None]:
@@ -9,17 +22,12 @@ def solve(puzzle_input: str) -> tuple[int, int | None]:
             break
         data.append(1 << (ord(char) - ord("a")))
 
-    answer_part_1 = - 1
-    for idx in range(3, len(data)):
-        if count_bits(data[idx - 3] | data[idx - 2] | data[idx - 1] | data[idx]) == 4:
-            answer_part_1 = idx + 1
-            break
-
-    answer_part_2 = None
+    answer_part_1 = first_unique_chars(data, 4)
+    answer_part_2 = first_unique_chars(data, 14)
     return answer_part_1, answer_part_2
 
 
-TEST_EXPECTED = (7, None)
+TEST_EXPECTED = (7, 19)
 TEST_INPUT = """\
 mjqjpqmgbljsphdztnvjfqwrcgsmlb
 """
